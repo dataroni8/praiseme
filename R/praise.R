09bff1c177015c85343f1061f59cc95b5bc2fd6b
Hang <HangRo@transport.local>
2018-11-21 15:13:12 +1100

    initialise the git repository, the first push of the praiseme package

---
+++ R/praise.R
@@ -0,0 +1,14 @@
+#' @title Praise someone
+#' @description Let's dole out praise
+#' @param name Specifies recipient of praise
+#'
+#' @examples praise("Nick")
+#' @examples praise("Holly")
+#' @export
+
+
+praise <- function (name = "Roni"){
+
+  paste0("you're the best, ", name, "!")
+
+}
